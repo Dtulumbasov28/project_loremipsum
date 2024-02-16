9b97939ab68dafc021c948af3c3bfc4734a29750
Denis Tulumbasov <tulumbasov.denis00@mail.ru>
2024-02-16 17:20:55 +0300

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,14 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+
+var slider = document.getElementById("order-range");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
